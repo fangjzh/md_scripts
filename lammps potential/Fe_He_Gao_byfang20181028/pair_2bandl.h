76a9f9ee2bcb1086e4f64f5e23893b558170851d
sam <fangjzh@foxmail.com>
2021-06-21 16:34:04 +0800

    3rd commit md script

--- lammps potential/Fe_He_Gao_byfang20181028/pair_2bandl.h
+++ lammps potential/Fe_He_Gao_byfang20181028/pair_2bandl.h
@@ -13,12 +13,12 @@
 
 #ifdef PAIR_CLASS
 
-PairStyle(twoband,Pair2BAND)
+PairStyle(twobandl,Pair2BANDL)
 
 #else
 
-#ifndef LMP_PAIR_2BAND_H
-#define LMP_PAIR_2BAND_H
+#ifndef LMP_PAIR_2BANDL_H
+#define LMP_PAIR_2BANDL_H
 
 #include <stdio.h>
 #include "pair.h"
@@ -26,28 +26,9 @@ PairStyle(twoband,Pair2BAND)
 namespace LAMMPS_NS {
 
 
-class Pair2BAND : public Pair {
+class Pair2BANDL : public Pair {
  public:
-
-//fjzzs potential parameter area start
-  double mass_w,mass_he;
-//fjzzs for tungsten-tungsten
-  double w_af1,w_af2,w_rho_rc,w_rho_0rc,w_rpc;
-  double *w_ap,*w_rp,*w_ao,*w_ro; 
-//fjzzs for helium-helium
-  double herm,c6,c8,c10,heaa,hea,heb,hed,hee,he_rpc;
-//fjzzs for tungsten-helum
-  double whe_rho_rc,whe_rpc,dNs,psi,rb;
-  double *whe_ap,*whe_rp,*whe_ao;
-//fjzzs for fermi function
-  double bf_w,rf_w,bf_he,rf_he,bf_whe,rf_whe;
-//fjzzs for ZBL
-  double ev,pi,epsil0,bohrads,exn,zeda2,zeda1;
-//  double betasa,betasa1,betasa2,rsa,rsa1,rsa2;
-//  double *bzaa,*bzba,*bzaa1,*bzba1,*bzaa2,*bzba2;
-  double betasa,rsa;
-  double *bzaa,*bzba;
-//fjzzs potential parameter area end
+  friend class FixSemiGrandCanonicalMC;   // Alex Stukowski option
 
   // public variables so USER-ATC package can access them
 
@@ -55,27 +36,33 @@ class Pair2BAND : public Pair {
 
   // potentials as array data
 
+  int nrho,nrhos,nr;
+  int nfrho,nfrhos,nrhor,nrhors,nz2r;
+  double **frho,**rhor,**frhos,**rhors,**z2r;
+  int *type2frho,**type2rhor,*type2frhos,**type2rhors,**type2z2r;
 
   // potentials in spline form used for force computation
 
-//  double dr,rdr,drho,rdrho,rhomax;
-//  double ***rhor_spline,***frho_spline,***z2r_spline;
+  double dr,rdr,drho,rdrho,rhomax,drhos,rdrhos,rhosmax;   // rdr = 1./dr
+  double ***rhor_spline,***frho_spline,***rhors_spline,***frhos_spline,***z2r_spline;
 
-  Pair2BAND(class LAMMPS *);
-  virtual ~Pair2BAND();
+  Pair2BANDL(class LAMMPS *);
+  virtual ~Pair2BANDL();
   virtual void compute(int, int);
-
+  void settings(int, char **);
   virtual void coeff(int, char **);
   void init_style();
   double init_one(int, int);
+  double single(int, int, int, int, double, double, double, double &);
+  virtual void *extract(const char *, int &);
+
   virtual int pack_forward_comm(int, int *, double *, int, int *);
   virtual void unpack_forward_comm(int, int, double *);
   int pack_reverse_comm(int, int, double *);
   void unpack_reverse_comm(int, int *, double *);
   double memory_usage();
+  void swap_eam(double *, double **);
 
-  void settings(int, char **);
-  
  protected:
   int nmax;                   // allocated size of per-atom arrays
   double cutforcesq;
@@ -83,46 +70,38 @@ class Pair2BAND : public Pair {
 
   // per-atom arrays
 
-  double *rho,*fp;
-  double *rhos,*fps;
-  
+  double *rho,*fp,*rhos,*fps;
+
   // potentials as file data
 
   int *map;                   // which element each atom type maps to
-
+/* 
+  struct Funcfl {
+    char *file;
+    int nrho,nrhos,nr;
+    double drho,drhos,dr,cut,mass;
+    double *frho,*frhos,*rhor,*rhors,*zr;
+  };
+  Funcfl *funcfl;
+  int nfuncfl;
+ */
   struct Setfl {
     char **elements;
-    int nelements;   //,nrho,nr;
-//    double drho,dr,cut;
+    int nelements,nrho,nrhos,nr;
+    double drho,drhos,dr,cut;
     double *mass;
-//   double **frho,**rhor,***z2r;
+    double **frho,**frhos,**rhor,**rhors,***z2r;
   };
-  Setfl *setfl;  
+  Setfl *setfl;
+
 
   virtual void allocate();
+  virtual void array2spline();
+  void interpolate(int, double, double *, double **);
+  void grab(FILE *, int, double *);
 
-  // function by fjz
-
-  double xH0(double);
-  double vee_pairww(double);
-  double vee_pairwhe(double);
-  double vee_pairhehe(double);
-  double vee_zbl(double,double,double);
-  double dvee_pairww(double);
-  double dvee_pairwhe(double);
-  double dvee_pairhehe(double);
-  double dvee_zbl(double,double,double);
-  
-  double rho_d(int,int,double);
-  double rho_s(int,int,double);
-  double drho_d(int,int,double);
-  double drho_s(int,int,double);
-  
-  double emb_d(int,double);
-  double emb_s(int,double);
-  double demb_d(int,double);
-  double demb_s(int,double);
-  
+  virtual void read_file(char *);
+  virtual void file2array();
 };
 
 }
